c337c0f160e1388224ea42b298fc7bdf1e45fb7f
LMG XENON <waleedaamir666@gmail.com>
2026-03-10 11:59:20 +0000

    Initial Commit

---
+++ Snotch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snotch
+//
+//  Created by Waleed Aamir on 10/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snotch/SnotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnotchApp.swift
+//  Snotch
+//
+//  Created by Waleed Aamir on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
