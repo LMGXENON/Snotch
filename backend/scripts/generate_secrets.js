342f6592d3bd65910ab667a84ce6a48b2da2cbad
LMG XENON <waleedaamir666@gmail.com>
2026-04-29 15:33:41 +0100

    Remove unused files: deleted prompter text, main HTML, and license data

--- backend/scripts/generate_secrets.js
+++
@@ -1,9 +0,0 @@
-import crypto from "node:crypto";
-
-function makeSecret(bytes = 32) {
-  return crypto.randomBytes(bytes).toString("hex");
-}
-
-console.log("JWT_SECRET=" + makeSecret(32));
-console.log("LICENSE_PEPPER=" + makeSecret(32));
-console.log("ADMIN_API_KEY=adm_" + makeSecret(24));
